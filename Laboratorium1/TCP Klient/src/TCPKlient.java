9476f3dcebc9cd981334cc80e125040e016fe4c9
Aylszen <aylszen@gmail.com>
2017-10-30 23:23:39 +0100

    Added SHOW and LIST commands

--- Laboratorium1/TCP Klient/src/TCPKlient.java
+++ Laboratorium1/TCP Klient/src/TCPKlient.java
@@ -5,31 +5,62 @@ public class TCPKlient {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		String sentence = null, usersentence = null;
+		String[] usersentenceArray;
 		Socket clientSocket = null;
 		BufferedReader inFromServer = null, inFromUser = null;
 		DataOutputStream outToServer = null;
-				
-		while (true){
-		clientSocket = new Socket("127.0.0.1", 6789);
-			
-		inFromUser = new BufferedReader(new InputStreamReader(System.in));
-		outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		usersentence = inFromUser.readLine();
-		outToServer.writeBytes(usersentence + "\n");
 
-		sentence = inFromServer.readLine();
-		System.out.println("FROM SERVER: " + sentence);
+		while (true) {
+			clientSocket = new Socket("127.0.0.1", 6789);
+
+			inFromUser = new BufferedReader(new InputStreamReader(System.in));
+			outToServer = new DataOutputStream(clientSocket.getOutputStream());
+			inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			usersentence = inFromUser.readLine();
+			usersentenceArray = usersentence.split(" ");
+			outToServer.writeBytes(usersentence + "\n");
+			sentence = inFromServer.readLine();
+			
+			checkServerSentence(sentence, clientSocket);
+			checkUserSentence(usersentenceArray, sentence);
+			}
 		
-		switch (sentence) {
-		case "QUIT":
-			clientSocket.close();
+	}
+
+	public static void checkUserSentence(String[] usersentenceArray, String sentence)
+	{
+		String[] temp = null;
+		switch (usersentenceArray[0].toUpperCase()) {
+		case "LIST":
+			temp = sentence.split("#");
+			for (int i =0;i<temp.length;i++)
+				System.out.println(temp[i]);
 			break;
-		case "null":
+		case "SHOW":
+			temp = sentence.split("#");
+			for (int i =0;i<temp.length;i++)
+				System.out.println(temp[i]);
+			break;
+		default:
+			System.out.println("FROM SERVER: " + sentence);
+			break;
+		}
+	}
+	public static void checkServerSentence(String sentence, Socket clientSocket) {
+		switch (sentence.toUpperCase()) {
+		case "QUIT":
+			try {
+				clientSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		case "NULL":
 			System.out.println("Server was closed!!");
 		default:
 			break;
-			}
 		}
 	}
+	
 }
--- Laboratorium1/TCP Server/src/TCPServer.java
+++ Laboratorium1/TCP Server/src/TCPServer.java
@@ -3,9 +3,10 @@ import java.net.*;
 
 class TCPServer {
 	public static void main(String argv[]) throws Exception {
-		String clientSentence;
+		String clientSentence, listSentence = null;
 		ServerSocket welcomeSocket = new ServerSocket(6789);
-
+		String[] clientSentenceArray;
+		File curDir = new File("C:\\Users\\Krzysiek\\repo\\Programowanie-Sieciowe\\Laboratorium1\\Documents");
 		while (true) {
 			Socket connectionSocket = welcomeSocket.accept();
 			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
@@ -13,27 +14,42 @@ class TCPServer {
 
 			clientSentence = inFromClient.readLine();
 			System.out.println("Received: " + clientSentence);
-			switch (clientSentence) {
+			clientSentenceArray = clientSentence.split(" ");
+			switch (clientSentenceArray[0].toUpperCase()) {
 			case "LIST":
-				File curDir = new File("C:\\Users\\ekrzkit\\repo\\Programowanie-Sieciowe\\Laboratorium1\\Documents");
-				getAllFiles(curDir, outToClient);
+				outToClient.writeBytes(getAllFiles(curDir) + "\n");
 				break;
 			case "SHUTDOWN":
 				connectionSocket.close();
 				break;
+			case "SHOW":
+				FileReader file = new FileReader(
+						curDir.getAbsolutePath().replaceAll("\\\\", "/") + "/" + clientSentenceArray[1]);
+				String line, allLines = "";
+				BufferedReader br = new BufferedReader(file);
+				while ((line = br.readLine()) != null) {
+					allLines += line;
+					allLines += "#";
+				} 
+				//allLines.replaceAll("\n", "#");
+				outToClient.writeBytes(allLines +"\n");
+				file.close();
+				break;
 			default:
 				outToClient.writeBytes(clientSentence + "\n");
 				break;
+
 			}
 		}
 	}
 
-	private static void getAllFiles(File curDir, DataOutputStream outToClient) throws IOException {
-
+	private static String getAllFiles(File curDir) throws IOException {
+		String listSentence = "Lista plikow:#";
 		File[] filesList = curDir.listFiles();
 		for (File f : filesList) {
 			if (f.isFile())
-				outToClient.writeBytes(f.getName() + "\n");
+				listSentence += (f.getName() + "#");
 		}
+		return listSentence;
 	}
 }
